38c0fe398b03525b7b8d83f984baad349bc1c3ed
chetanv <chetanv@9c654c6e-0788-31ec-e572-95655c417402>
2010-08-11 17:45:45 +0000

    Bug: #1
      * The RoR project along with Simile stuff

---
+++ trunk/tl/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9880d10ca4a429adfda024e46f8ca763a6ab0af58b60a3ca11423e519add37c6f6a87ff2051331c6d36735902ee538821d5b0987f017eb9551ce816b7af75e88';
